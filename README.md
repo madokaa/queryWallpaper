61edab062f4352cdff36326025440a49fb9d2c29
Madola <i@gvip.xyz>
2021-01-21 10:26:41 +0800

    增加多显示器适配，优化功能

--- README.md
+++ README.md
@@ -11,13 +11,14 @@ pip3 install Pillow
 ```
 
 ### C. 使用方法
-1. 修改文件中的 `wallpaper_dir` 为当前设置的桌面壁纸路径
-2. 查找并打开当前壁纸 (第一次打开或增加新文件后会索引文件，会比较慢)
+1. 修改文件中的`wallpaper_dir`为当前设置的桌面壁纸路径
+2. 查找并打开当前壁纸，下面命令二选一 (第一次打开或增加新文件后会索引文件，会比较慢)
 ``` shell
 python3 ./main.py
+python3 ./main.py -n \\.\DISPLAY1
 ```
 
-### D. 使用 `AHK` 设置快捷键运行
+### D. 使用`AHK`设置快捷键运行
 通过ahk软件可以方便的用快捷键执行脚本，下面为实例脚本（请见路径替换为自己的实际路径）
 ``` ahk
 ; ----- 在桌面按 ctrl+W 建打开当前桌面壁纸文件 -----
@@ -25,8 +26,13 @@ python3 ./main.py
 {
     MouseGetPos, mX, mY, mId
     WinGetClass, className, ahk_id %mId%
-    if (className = "WorkerW") {
-        Run .\venv\Scripts\python.exe ./wallpaper/main.py, D:\Code\Python, Hide
+    SysGet, MonitorCount, MonitorCount
+    Loop %MonitorCount% {
+        SysGet, MonitorName, MonitorName, %A_Index%
+        SysGet, Monitor, Monitor, %A_Index%
+        if (className = "WorkerW" and mX >= MonitorLeft and mX <= MonitorRight and mY >= MonitorTop and mY <= MonitorBottom) {
+            Run .\venv\Scripts\python.exe ./wallpaper/main.py -n %MonitorName%, D:\Code\Python, Hide
+        }
     }
     Return
 }
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import getopt
+import glob
+import sys
 from os import path, listdir, system
 from functools import reduce
 from multiprocessing import Pool
@@ -32,7 +35,6 @@ def get_file_info(file_name):
 
 def init_index(img_dir):
     """ 初始化壁纸文件索引 """
-    global ts
     data_path = f'{path.dirname(__file__)}/wallpaper_data.json'
     included_extensions = ['jpg', 'jpeg', 'bmp', 'png']
     file_names = [f'{img_dir}/{fn}' for fn in listdir(img_dir) if any(fn.endswith(ext) for ext in included_extensions)]
@@ -47,30 +49,56 @@ def init_index(img_dir):
     for item in wallpaper_data:
         w_size[item[0]] = item[2]
     file_names = list(filter(lambda fn: w_size.get(fn) is None or w_size.get(fn) != path.getsize(fn), file_names))
+
+    # 索引文件(大于等于32个图片启用多进程)
     if len(file_names) == 0:
         return
-
-    # 多线程索引文件
-    pool = Pool()
-    wallpaper_data += pool.map(get_file_info, file_names)
-    pool.close()
-    pool.join()
+    elif len(file_names) < 32:
+        wallpaper_data = [get_file_info(n) for n in file_names]
+    else:
+        with Pool() as pool:
+            wallpaper_data += pool.map(get_file_info, file_names)
 
     with open(f'{path.dirname(__file__)}/wallpaper_data.json', 'w', encoding='utf-8') as f:
         f.write(json.dumps(wallpaper_data, ensure_ascii=False, indent=2))
 
 
-def query_wallpaper():
+def query_wallpaper(monitor_id: int):
     """ 查找并打开当前使用壁纸 """
-    h = phash(Image.open(path.expandvars('%APPDATA%/Microsoft/Windows/Themes/TranscodedWallpaper')))
+    if monitor_id is None:
+        file_paths = glob.glob(path.expandvars(f'%APPDATA%/Microsoft/Windows/Themes/Transcoded_*'))
+    else:
+        file_paths = [path.expandvars(f'%APPDATA%/Microsoft/Windows/Themes/Transcoded_{monitor_id:03d}')]
+    hs = [phash(i) for i in file_paths]
+
     with open(f'{path.dirname(__file__)}/wallpaper_data.json', 'r', encoding='utf-8') as f:
         wallpaper_data = json.load(f)
+
     for f in wallpaper_data:
-        if hamming_distance(h, f[1]) <= 5:
-            print(hamming_distance(h, f[1]), f, h)
-            system(f'start "" "{f[0]}"')
+        for h in hs:
+            if hamming_distance(h, f[1]) <= 3:  # 默认5
+                print(hamming_distance(h, f[1]), f, h)
+                system(f'start "" "{f[0]}"')
+
+
+def main(argv):
+    init_index(wallpaper_dir)
+
+    monitor_name = ''
+    try:
+        opts, args = getopt.getopt(argv, "hn:", ["name=", ])
+    except getopt.GetoptError:
+        print('main.py -n <monitor_name>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('main.py -n <monitor_name>')
+            break
+        elif opt in ("-n", "--name"):
+            monitor_name = arg  # r'\\.\DISPLAY*'
+    monitor_id = int(monitor_name[-1:]) - 1 if monitor_name != '' else None
+    query_wallpaper(monitor_id)
 
 
 if __name__ == '__main__':
-    init_index(wallpaper_dir)
-    query_wallpaper()
+    main(sys.argv[1:])
